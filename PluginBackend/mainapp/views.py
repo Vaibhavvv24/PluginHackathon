861cd6ac2a9da6dd1fadec5ef8bb0f5acc3c048a
valmikGit <quantumcomputingiiitb@gmail.com>
2024-12-11 01:16:32 +0530

    added home

--- PluginBackend/mainapp/views.py
+++ PluginBackend/mainapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from mainapp.models import User, UserHistory, Report, VideoAudio
+from mainapp.serializers import UserSerializer, UserHistorySerializer, ReportSerializer, VideoAudioSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
+
+@api_view(['POST', 'GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def users(request: HttpRequest) -> Response:
+    """"""
+
+def home(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>HOME<\h1>")
