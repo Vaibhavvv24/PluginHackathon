bb2a9783665e6b5b58288e84fa1f50d9df8c3a7c
valmikGit <quantumcomputingiiitb@gmail.com>
2024-12-11 22:59:56 +0530

    file saving using default extensions.

--- PluginBackend/mainapp/models.py
+++ PluginBackend/mainapp/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager, Permission
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from django.utils.text import slugify
+import os
 class CustomAccountManager(BaseUserManager):
     def create_superuser(self, email, username, password, **other_fields):
         other_fields.setdefault('is_staff', True)
@@ -49,6 +51,25 @@ class VideoAudio(models.Model):
     audio_file = models.FileField(upload_to='media/audios/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        # Ensure video and audio file extensions are correct
+        if self.video_file:
+            # Get the file name and extension of the video
+            video_filename, video_extension = os.path.splitext(self.video_file.name)
+            # If the extension is not .mp4, change it to .mp4
+            if video_extension.lower() != '.mp4':
+                self.video_file.name = f"{slugify(self.title)}.mp4"  # Set to .mp4 extension
+
+        if self.audio_file:
+            # Get the file name and extension of the audio
+            audio_filename, audio_extension = os.path.splitext(self.audio_file.name)
+            # If the extension is not .mp3, change it to .mp3
+            if audio_extension.lower() != '.mp3':
+                self.audio_file.name = f"{slugify(self.title)}.mp3"  # Set to .mp3 extension
+
+        # Call the original save method
+        super().save(*args, **kwargs)
 
 class Report(models.Model):
     id = models.AutoField(primary_key=True)
